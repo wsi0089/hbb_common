b20dc5404b9d61ce35919bd25c324b6827a7fc02
你的实际名字 <>
2025-07-30 22:10:49 +0800

    api

--- src/config.rs
+++ src/config.rs
@@ -100,8 +100,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["w2602.cn"];
+pub const RS_PUB_KEY: &str = "3Mv9mZQDgPZ1fBKyeYrGW6Aw2dKmKd124q3ChZUcHNk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
